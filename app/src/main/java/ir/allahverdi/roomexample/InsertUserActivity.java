3e28afa5aeb177af8a87cad085942cf46e07fbeb
alroid <al.allahverdi@gmail.com>
2020-12-30 11:52:49 +0330

    init insert user views

--- app/src/main/java/ir/allahverdi/roomexample/InsertUserActivity.java
+++ app/src/main/java/ir/allahverdi/roomexample/InsertUserActivity.java
@@ -3,12 +3,31 @@ package ir.allahverdi.roomexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class InsertUserActivity extends AppCompatActivity {
+
+    EditText et_firsrName,
+            et_lastName,
+            et_email,
+            et_phone;
+    Button btn_insert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_user);
+
+        initViews();
     }
+
+    private void initViews() {
+        et_firsrName = findViewById(R.id.et_firsrName);
+        et_lastName = findViewById(R.id.et_lastName);
+        et_email = findViewById(R.id.et_email);
+        et_phone = findViewById(R.id.et_phone);
+        btn_insert = findViewById(R.id.btn_insert);
+    }
+
 }
