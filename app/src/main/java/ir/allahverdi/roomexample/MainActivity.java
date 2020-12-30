ac259709a8925e0203e61be14f4c5d4c8f10ad65
alroid <al.allahverdi@gmail.com>
2020-12-30 11:42:14 +0330

    add insert option menu

--- app/src/main/java/ir/allahverdi/roomexample/MainActivity.java
+++ app/src/main/java/ir/allahverdi/roomexample/MainActivity.java
@@ -2,8 +2,10 @@ package ir.allahverdi.roomexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("Insert").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent intent = new Intent(MainActivity.this, InsertUserActivity.class);
+                startActivity(intent);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 }
