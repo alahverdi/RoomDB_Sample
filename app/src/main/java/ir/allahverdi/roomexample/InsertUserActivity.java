519a5aea9349c6183d69730aef32f9000d56a282
alroid <al.allahverdi@gmail.com>
2020-12-30 10:53:40 +0330

    create insert user activity

--- app/src/main/java/ir/allahverdi/roomexample/InsertUserActivity.java
+++ app/src/main/java/ir/allahverdi/roomexample/InsertUserActivity.java
@@ -3,13 +3,12 @@ package ir.allahverdi.roomexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.Menu;
 
-public class MainActivity extends AppCompatActivity {
+public class InsertUserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_insert_user);
     }
 }
