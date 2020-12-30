f04cf4a0eacba26c56111c9d26a5ee6a9aebee3b
alroid <al.allahverdi@gmail.com>
2020-12-30 15:25:35 +0330

    define getDataFtomDb in main activity

--- app/src/main/java/ir/allahverdi/roomexample/MainActivity.java
+++ app/src/main/java/ir/allahverdi/roomexample/MainActivity.java
@@ -3,15 +3,20 @@ package ir.allahverdi.roomexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import ir.allahverdi.roomexample.database.MyRoomDb;
 import ir.allahverdi.roomexample.entity.User;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<User> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +38,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private List<User> getDataFromDb() {
-        return null;
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                list = MyRoomDb.getInstance(MainActivity.this)
+                        .userDAO()
+                        .getAll();
+            }
+        });
+        return list;
     }
 }
